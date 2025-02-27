c80a46b5fe70b155d6542b966a2da8315bb54244
ANOUD <a20nood@gmail.com>
2025-02-27 09:27:10 +0300

    Initial Commit

---
+++ T5/T5App.swift
@@ -0,0 +1,17 @@
+//
+//  T5App.swift
+//  T5
+//
+//  Created by Alanoud Alamrani on 28/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct T5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
